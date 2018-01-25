f90e1dc63cd8065c3bd6165390feaa13b11d11be
superKalo <superkalo@devlabs.bg>
2018-01-25 22:30:42 +0200

    Change: update author slug

--- content/plugins/edit-author-slug/js/edit-author-slug.js
+++ content/plugins/edit-author-slug/js/edit-author-slug.js
@@ -1,4 +1,6 @@
-jQuery( document ).ready(function( $ ) {
+/* jshint devel: true */
+
+jQuery( document ).ready( function( $ ) {
 
 	// Front show/hide.
 	if ( $( 'input[name="_ba_eas_remove_front"]' ).prop( 'checked' ) ) {
@@ -9,24 +11,24 @@ jQuery( document ).ready(function( $ ) {
 	$( 'input[name="_ba_eas_remove_front"]' ).on( 'click', function() {
 		if ( $( this ).prop( 'checked' ) ) {
 			$( 'span[class="eas-demo-author-base-front"]' ).fadeOut( 'fast', function() {
-$( this ).addClass( 'hidden' );
-});
+				$( this ).addClass( 'hidden' );
+			});
 		} else {
 			$( 'span[class="eas-demo-author-base-front hidden"]' ).fadeIn( 'slow', function() {
-$( this ).removeClass( 'hidden' );
-});
+				$( this ).removeClass( 'hidden' );
+			});
 		}
 	});
 
 	// Make example nicenames clickable.
-	$( 'input[name="ba_eas_author_slug"]' ).click(function() {
+	$( 'input[name="ba_eas_author_slug"]' ).click( function() {
 		if ( 'ba_eas_author_slug_custom_radio' !== $( this ).attr( 'id' ) ) {
 			$( 'input[name="ba_eas_author_slug_custom"]' ).val( $( this ).val() ).text( $( this ).siblings( 'span' ).text() );
 		}
 	});
 
 	// If focus moves to the custom author slug input, select the radio.
-	$( 'input[name="ba_eas_author_slug_custom"]' ).focus(function() {
+	$( 'input[name="ba_eas_author_slug_custom"]' ).focus( function() {
 		$( '#ba_eas_author_slug_custom_radio' ).attr( 'checked', 'checked' );
 	});
 
@@ -37,12 +39,12 @@ $( this ).removeClass( 'hidden' );
 	$( 'input[class="eas-checkbox"]' ).on( 'click', function() {
 		if ( $( this ).prop( 'checked' ) ) {
 			$( this ).parents( 'tr' ).next( 'tr' ).fadeIn( 'slow', function() {
-$( this ).removeClass( 'hidden' );
-});
+				$( this ).removeClass( 'hidden' );
+			});
 		} else {
 			$( this ).parents( 'tr' ).next( 'tr' ).fadeOut( 'fast', function() {
-$( this ).addClass( 'hidden' );
-});
+				$( this ).addClass( 'hidden' );
+			});
 		}
 	});
 });
